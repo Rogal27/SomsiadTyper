b41be17f0a0c06096670705850ad4344f710f16e
Michal Rogala <gm.rogala@gmail.com>
2020-06-03 19:03:32 +0200

    Change post authentication to post confirmation

--- cognito-lambda/post-confirmation/app.js
+++ cognito-lambda/post-confirmation/app.js
@@ -12,13 +12,8 @@
  */
 
 exports.handler = (event, context, callback) => {
-
-    // Send post authentication data to Cloudwatch logs
-    console.log ("Authentication successful");
-    console.log ("Trigger function =", event.triggerSource);
-    console.log ("User pool = ", event.userPoolId);
-    console.log ("App client ID = ", event.callerContext.clientId);
-    console.log ("event = ", event);
+    // Send post confirmation data to Cloudwatch logs
+    console.log(event);
 
     // Return to Amazon Cognito
     callback(null, event);
