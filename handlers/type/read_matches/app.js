ff9fb3b2199629f3de690bd2b10acc95dce03bf4
Irek Stanicki <ireneuszstanicki@gmail.com>
2020-06-06 11:49:26 +0200

    Add lambda to read matches to type

---
+++ handlers/type/read_matches/app.js
@@ -0,0 +1,42 @@
+const dynamodb = require('aws-sdk/clients/dynamodb');
+const docClient = new dynamodb.DocumentClient();
+
+const tables = require('/opt/dbtables');
+const tableName = tables.MATCHES;
+
+exports.lambdaHandler = async (event, context, callback) => {
+    if (event.httpMethod !== 'POST') {
+        throw new Error(`postMethod only accepts POST method, you tried: ${event.httpMethod} method.`);
+    }
+
+    console.info('received:', event);
+    var requestBody = JSON.parse(event.body);
+    var contest_id = requestBody.contest_id;
+    console.info(contest_id)
+    var today = new Date();
+
+ 
+    var params = {
+        TableName : tableName,
+        FilterExpression: "#contest_id = :contest_id and #date <= :date",
+        ExpressionAttributeNames: {
+            "#contest_id": "contest_id",
+            "#date": "date"
+        },
+        ExpressionAttributeValues: { ":contest_id": contest_id, "date": today }
+    
+    };
+
+    // Call DynamoDB to read the item to the table
+    const result = await docClient.scan(params).promise();
+    
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+            result
+        }),
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
+};
