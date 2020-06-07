bb371edca468d5e4ab3a1d23034050658eb3158e
Michal Rogala <gm.rogala@gmail.com>
2020-06-07 21:43:27 +0200

    Small changes in ui

--- website/js/user.js
+++ website/js/user.js
@@ -1,18 +1,21 @@
 const ApiURL = _config.api.invokeUrl;
 
 var authToken;
-somsiadTyper.authToken.then(function setAuthToken(token) {
+somsiadTyper.authToken
+  .then(function setAuthToken(token) {
     if (token) {
-        authToken = token;
+      authToken = token;
     } else {
-        window.location.href = '/login.html';
+      window.location.href = "/login.html";
     }
-}).catch(function handleTokenError(error) {
+  })
+  .catch(function handleTokenError(error) {
     alert(error);
-    window.location.href = '/login.html';
-});
+    window.location.href = "/login.html";
+  });
 
 $(document).ready(function () {
+  $("#fileInput").submit();
   setUser();
 });
 
@@ -37,4 +40,8 @@ function completeSetUserRequest(response) {
 
   $("#name").text(response.name);
   $("#profile_photo").attr("src", response.photo);
+}
+
+function submitPhotoForm() {
+  $()
 }
