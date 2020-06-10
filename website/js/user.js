88ed44e72dfe189139fed459d4731f2e1c427359
Irek Stanicki <ireneuszstanicki@gmail.com>
2020-06-10 19:52:07 +0200

    Read user results on details view

--- website/js/user.js
+++ website/js/user.js
@@ -84,10 +84,101 @@ function completeReadContestRequest(response){
       $('#matchContestSelect').append(`<option value="${element.contest_id}">${element.name}</option>`);
   }
 
-  ReadMyMatchesResults();
+  ReadMatchesResults();
 }
 
+function ReadMatchesResults(){
+  var contest = $('#matchContestSelect').val();
+  if(contest == null)
+      return;
 
+  startLoading();
+  var user_id = getUserIdFromURL();
+  var dataToSend;
+  if(user_id){
+    dataToSend = {
+      contest_id: contest,
+      user_id
+    }
+  }
+  else{
+    dataToSend = {
+      contest_id: contest
+    }
+  }
+
+  $.ajax({
+      method: 'POST',
+      url: ApiURL + "/readscores",
+      headers: {
+          Authorization: authToken
+      },
+      data:JSON.stringify(dataToSend),
+      success: completeReadMyMatchesResultsRequest,
+      error: function ajaxError(jqXHR, textStatus, errorThrown) {
+          stopLoading();
+          $("#errorLabel").text("Błąd podczas odczytu wyników");
+          $("#alertDiv").css("display","block");
+      }
+  });
+}
+
+function completeReadMyMatchesResultsRequest(response){
+  stopLoading();
+
+  var table = document.getElementById('matches_table');
+  var rowCount = table.rows.length;
+  for (var i = 1; i < rowCount; i++) {
+      table.deleteRow(1);
+  }
+
+  var size = response.result.length;
+
+  for(var i=0; i<size; i++){
+      element = response.result[i];
+      var date = new Date(element.date);
+      var dd = date.getDate();
+      var mm = date.getMonth()+1; 
+      var yyyy = date.getFullYear();
+      if(dd<10) 
+          dd='0'+dd;
+      if(mm<10) 
+          mm='0'+mm;
+      var resultDate = dd+'-'+mm+'-'+yyyy;
+
+      var minutes = date.getMinutes();
+      var hours = date.getHours();
+      if(minutes<10)
+          minutes='0'+minutes;
+      if(hours<10)
+          hours='0'+hours;
+      var resultTime = hours+":"+minutes+" "+resultDate;
+
+      var row = table.insertRow(1);
+
+      var result = "<center>-</center>";
+      if(element.away_team_score && element.home_team_score)
+          result = element.home_team_score + " - " + element.away_team_score;
+      var myType = "<center>-</center>";
+      if(element.away_team_type && element.home_team_type)
+          myType = element.home_team_type + " - " + element.away_team_type;
+      var points = "<center>-</center>";
+      if(element.points == 0 || element.points)
+          points = element.points;
+
+      var cell1 = row.insertCell(0);
+      var cell2 = row.insertCell(1);
+      var cell3 = row.insertCell(2);
+      var cell4 = row.insertCell(3);
+      var cell5 = row.insertCell(4);
+
+      cell1.innerHTML = element.home_team + "-" + element.away_team;
+      cell2.innerHTML = resultTime;
+      cell3.innerHTML = myType;
+      cell4.innerHTML = result;
+      cell5.innerHTML = points;
+  }
+}
 
 function startLoading(){
   $("#spinner").css("display","block");
