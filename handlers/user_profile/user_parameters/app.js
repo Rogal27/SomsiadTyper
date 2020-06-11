6aef8b47c7d75b444bb1be8ce31c485cad1f9a82
Bartek Truszkowski <bartet24@gmail.com>
2020-06-11 22:00:57 +0200

    return from claims

--- handlers/user_profile/user_parameters/app.js
+++ handlers/user_profile/user_parameters/app.js
@@ -13,40 +13,10 @@ exports.lambdaHandler = async (event, context) => {
 
   console.info("Received:", event);
 
-  var current_user_id = event.requestContext.authorizer.claims.sub;
-
-  var requestBody = JSON.parse(event.body);
-
-  var requested_user_id;
-  if (requestBody && requestBody.user_id) requested_user_id = requestBody.user_id;
-  else requested_user_id = current_user_id;
-
-  var params = {
-    TableName: tableUsers,
-    Key: {
-      user_id: requested_user_id,
-    },
-  };
-
-  var user_data = await docClient.get(params).promise();
-
-  if (!user_data) {
-    console.info("User with requested ID not found");
-    return response.GetResponse(400, { message: "User with requested ID not found" });
-  }
-
-  if (requested_user_id == current_user_id) {
-    console.info("Returning data for user", user_data);
-    return response.GetResponse(200, {
-      name: user_data.Item.name,
-      role: user_data.Item.role,
-    });
-  }
-
-  console.info("Returning data for other user", user_data);
+  var claims = event.requestContext.authorizer.claims;
 
   return response.GetResponse(200, {
-    name: user_data.Item.name,
-    role: user_data.Item.role,
+    name: claims.name,
+    role: claims.role,
   });
 };
